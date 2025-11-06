19b028ec341ad3c6157bc76d01b078a412c13821
Amad3uXs <davidmartveg@gmail.com>
2025-11-06 13:39:34 -0600

    Create Programa1c.c

---
+++ Programa1c.c
@@ -0,0 +1,25 @@
+/*David Alejandro Martínez Vega L´Englisse
+practica 10 ejercicio 1c*/
+
+#include<stdio.h>
+int main()
+{
+ int matriz[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+ int i, j;
+ printf("Imprimir Matriz\n");
+ i=0;
+ do //Representa al renglón del arreglo
+ {
+   j=0;
+  do //Representa a la columna del arreglo
+   {
+     printf("%d, ",matriz[i][j]);
+     j++;
+   }
+   while (j<3);
+   printf("\n");
+   i++;
+ }
+ while(i<3);
+ return 0;
+}
